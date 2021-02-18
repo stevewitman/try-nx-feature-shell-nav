b1f3b2a1f8a4adf6b882a32e1f074c517ca7b54e
Steve Witman <stevewitman@gmail.com>
2021-02-18 11:27:43 -0700

    add lib: shared-material

---
+++ libs/shared/material/src/lib/shared-material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatToolbarModule],
+})
+export class SharedMaterialModule {}
